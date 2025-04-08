95f3eb52c1ad7a0e6daf8758aaf9303cb0bc0783
jay-tatvasoft <jay.vadgama@tatvasoft.com>
2025-04-08 09:57:02 +0530

    Modifiled global constants

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,16 +1,21 @@
-export const ROUTES =  {
-    LOGIN  : "/login",
-    SIGN_UP : "/sign-up",
-    HOME : "/",
-    USER_PROFILE : "/user/profile",
-    ADMIN : {
-        DASHBOARD : "/admin/dashboard",
-        EVENTS : "/admin/event"
+export const ROUTES = {
+    LOGIN: "/login",
+    SIGN_UP: "/sign-up",
+    HOME: "/",
+    USER_PROFILE: "/user/profile",
+    ADMIN: {
+        DASHBOARD: "/admin/dashboard",
+        EVENTS: "/admin/event"
     }
 }
 
 
 export const ADMIN_SIDEBAR_ITEMS = [
-    { id : 1, title : "Dashboard", route : ROUTES.ADMIN.DASHBOARD, icon : "/assets/DashboardIcon.svg"},
-    { id : 2, title : "Events", route : ROUTES.ADMIN.EVENTS, icon : "/assets/EventsIcon.svg"},
-] 
+    { id: 1, title: "Dashboard", route: ROUTES.ADMIN.DASHBOARD, icon: "/assets/DashboardIcon.svg" },
+    { id: 2, title: "Events", route: ROUTES.ADMIN.EVENTS, icon: "/assets/EventsIcon.svg" },
+]
+
+export enum ROLE {
+    Admin = "admin",
+    User = "user",
+}
