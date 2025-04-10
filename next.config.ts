606fd08ad00bb652e5a218a83c4f7a93f0b27308
devanshi <devanshifour@gmail.com>
2025-04-10 14:23:38 +0530

    Next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // for image support with static export
+  },
 };
 
 export default nextConfig;
