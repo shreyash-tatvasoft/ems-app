ab84f956532202d5daa93eba8142017c9eef5ef9
shreyash-tatvasoft <shreyash.zala@tatvasoft.com>
2025-05-06 18:30:41 +0530

    Enhancemets Fixes

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -127,7 +127,7 @@ export const CATOGORIES_ITEMS = [
 export const SIGN_UP_IMAGE_BANNER_LINK = "https://img.freepik.com/free-vector/privacy-policy-concept-illustration_114360-7853.jpg?semt=ais_hybrid&w=740"
 export const LOG_IN_IMAGE_BANNER_LINK = "https://img.freepik.com/free-vector/sign-page-abstract-concept-illustration_335657-2242.jpg?semt=ais_hybrid&w=740"
 export const CONTACT_US_IMAGE_BANNER_LINK = "https://d2r3fkmprkayl1.cloudfront.net/Eventtitans-new-img/EventTitans-inner/inner-page-img15.png"
-
+export const FAQ_BANNER_LINK = "https://backend-23.nubeseo.es/wp-content/uploads/2024/12/faqs-que-es.jpg"
 
 export enum ROLE {
     Admin = "admin",
